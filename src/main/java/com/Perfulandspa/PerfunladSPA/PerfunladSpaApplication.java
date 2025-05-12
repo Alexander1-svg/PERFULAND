290d3955aad9cadb069d1e4d8d79d35f24d72b1f
Valparaiso <valparaiso@duoc.acad>
2025-05-12 12:21:17 -0400

    Creando proyecto

---
+++ src/main/java/com/Perfulandspa/PerfunladSPA/PerfunladSpaApplication.java
@@ -0,0 +1,13 @@
+package com.Perfulandspa.PerfunladSPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerfunladSpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerfunladSpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Perfulandspa/PerfunladSPA/PerfunladSpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Perfulandspa.PerfunladSPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerfunladSpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
